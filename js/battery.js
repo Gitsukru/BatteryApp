ac35865b1d570250ee4e2b0982a8e959ce53f465
Gitsukru <37087893+Gitsukru@users.noreply.github.com>
2019-01-25 23:52:55 +0100

    .
    .

---
+++ js/battery.js
@@ -0,0 +1,25 @@
+class Battery {
+    constructor (pLevel){
+        this.level = pLevel;
+        this.full = 100;
+        this.empty = 0;
+    }
+    getLevel (){
+        return this.level;
+    }
+
+    getFul (){
+        return this.full;
+    }
+    getEmpty (){
+        return this.empty;
+    }
+
+    charging (){
+        this.level
+    }
+
+    dischargin (){
+
+    }
+}
